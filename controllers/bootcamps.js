3dba723468b15925a57760dee290bd8f2330675a
marb <marb61a@gmail.com>
2019-10-28 12:59:46 +0000

    added  bootcamp controller methods

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,8 +1,33 @@
 // Middleware functions which will remove some logic
 // from within route declarations
+
+// @desc            Get all bootcamps
+// @route           GET /api/v1/bootcamps
+// @access          Public
 exports.getBootcamps = (req, res, next) => {
     res.status(200).json({
         success: true,
         msg: 'Show all bootcamps'
     });
 };
+
+// @desc            Get a single bootcamp
+// @route           GET /api/v1/bootcamps/:id
+// @access          Public
+exports.getBootcamp = (req, res, next) => {
+    res.status(200).json({
+        success: true,
+        msg: `Show bootcamp ${req.params.id}`
+    });
+};
+
+// @desc            Create a single bootcamp
+// @route           POST /api/v1/bootcamps
+// @access          Private
+exports.createBootcamp = (req, res, next) => {
+    res.status(200).json({
+        success: true,
+        msg: 'Create new bootcamp'
+    });
+};
+
