524fe24669f3305430607cb684dcc9eaba357bfa
marb <marb61a@gmail.com>
2019-10-29 20:20:50 +0000

    added route methods to the bootcamp controller file and imported to routes file

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -31,3 +31,22 @@ exports.createBootcamp = (req, res, next) => {
     });
 };
 
+// @desc            Update a bootcamp
+// @route           PUT /api/v1/bootcamps/:id
+// @access          Private
+exports.updateBootcamp = (req, res, next) => {
+    res.status(200).json({
+        success: true,
+        msg: `Update bootcamp ${req.params.id}`
+    });
+};
+
+// @desc            Delete bootcamp
+// @route           DELETE /api/v1/bootcamps/:id
+// @access          Private
+exports.deleteBootcamp = (req, res, next) => {
+    res.status(200).json({
+        success: true,
+        msg: `Delete bootcamp ${req.params.id}`
+    });
+};
--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,38 +1,11 @@
 const express = require("express");
 
+// Using destructuring to bring in routes from the bootcamp controller file
+const{
+    getBootcamps, getBootcamp, createBootcamp, updateBootcamp, deleteBootcamp
+} = require('../controllers/bootcamps');
+
 // Initialise the express router
 const router = express.Router();
-
-router.get('/', (req, res) => {
-    
-});
-
-router.get('/:id', (req, res) => {
-    res.status(200).json({
-        success: true,
-        msg: `Show bootcamp ${req.params.id}`
-    });
-});
-
-router.post('/', (req, res) => {
-    res.status(200).json({
-        success: true,
-        msg: 'Create new bootcamp'
-    });
-});
-
-router.put('/', (req, res) => {
-    res.status(200).json({
-        success: true,
-        msg: `Update bootcamp ${req.params.id}`
-    });
-});
-
-router.delete('/', (req, res) => {
-    res.status(200).json({
-        success: true,
-        msg: `Delete bootcamp ${req.params.id}`
-    });
-});
 
 module.exports = router;
