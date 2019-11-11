afd303419a013dff2f1239da625ea9c6edd1a043
marb <marb61a@gmail.com>
2019-11-11 21:06:22 +0000

    Updated bootcamps validation

--- middleware/error.js
+++ middleware/error.js
@@ -11,7 +11,21 @@ const errorHandler = (err, req, res, next) => {
     // Mongoose bad ObjectId
     if(err.name === "CastError") {
         const message = `Resource with id of ${err.value} has not been found`;
-        error = new ErrorREsponse(message, 404);
+        error = new ErrorResponse(message, 404);
+    }
+
+    // Mongoose duplicate key, this will check the code rather than the
+    // error name which is MongoError which is shared by several errors
+    if(err.code === 11000) {
+        const message = "Duplicate field value enetered";
+        error = new ErrorResponse(message, 400);
+    }
+
+    // Mongoose validation error
+    if(err.name === "ValidationError") {
+        const message = Object.values(err.errors)
+            .map(val => val.message);
+            error = new ErrorResponse(message, 400);
     }
 
     res.status(error.statusCode || 500).json({
