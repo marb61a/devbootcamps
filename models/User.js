9a61344200801e5e52822606d4bc6edb7efd5511
marb <marb61a@gmail.com>
2019-11-15 20:19:31 +0000

    added initial user model

---
+++ models/User.js
@@ -0,0 +1,22 @@
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please add a name']
+    },
+    email: {
+        type: String,
+        required: [true, 'Please add an email'],
+        unique: true,
+        match: [
+            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+            'Please add a valid email'
+        ]
+    }
+});
+
+module.exports = mongoose.model('User', UserSchema);
