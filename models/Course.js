791541bc7147d7601dc742cba26ac182b8dbf473
marb <marb61a@gmail.com>
2019-11-18 20:32:06 +0000

    updated  Course model

--- models/Course.js
+++ models/Course.js
@@ -5,6 +5,32 @@ const CourseSchema = mongoose.Schema({
         type: String,
         trim: true,
         required: [true, 'Please add a course title']
+    },
+    description: {
+        type: String,
+        required: [true, 'Please add a description']
+    },
+    weeks: {
+        type: String,
+        required: [true, 'Please add number of weeks']
+    },
+    tuition: {
+        type: Number,
+        required: [true, 'Please add a tuition fee']
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    bootcamp: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Bootcamp',
+        required: true
+    },
+    user: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        required: true
     }
 });
 
