3c7c9a462a1ee35c2fa60db71e705b72bd2da775
marb <marb61a@gmail.com>
2019-11-24 12:11:51 +0000

    added logout user controller message

--- controllers/auth.js
+++ controllers/auth.js
@@ -46,3 +46,19 @@ exports.login = asyncHandler(async(req, res, next) => {
 
     sendTokenResponse(user, 200, res);
 });
+
+// @desc            Logout User
+// @route           POST /api/v1/auth/logout
+// @access          Private
+exports.logout = asyncHandler(async(req, res, next) => {
+    res.cookie("token", "none", {
+        expires: new Date(Date.now() + 10 * 1000),
+        httpOnly: true
+    });
+
+    res.status(200)
+        .json({
+            success: true,
+            data: {}
+        });
+});
