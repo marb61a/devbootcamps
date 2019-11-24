dda7257e50ddccea209708958ec3d33871b25608
marb <marb61a@gmail.com>
2019-11-24 12:22:28 +0000

    updated db seeder file

--- seeder.js
+++ seeder.js
@@ -22,10 +22,35 @@ const bootcamps = JSON.parse(
     fs.readFileSync(`${__dirname}/_data/bootcamps.json`, `utf-8`)
 );
 
+const courses = JSON.parse(
+    fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
+  );
+  
+  const users = JSON.parse(
+    fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8')
+  );
+
 // Import into the database
 const importData = async() => {
-    try{
+    try{ 
         await Bootcamp.create(bootcamps);
+        await Course.create(courses);
+        await User.create(users);
+
+        console.log('Data Imported...'.green.inverse);
+        process.exit();
+    } catch(err) {
+        console.error(err);
+    }
+};
+
+// Delete data
+const deleteData = async () => {
+    try {
+        await Bootcamp.deleteMany();
+        await Course.deleteMany();
+        await User.deleteMany();
+        
     } catch(err) {
         console.error(err);
     }
