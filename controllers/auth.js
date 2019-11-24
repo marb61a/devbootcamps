000311192f55933290ec1cd8feb47af1525b5101
marb <marb61a@gmail.com>
2019-11-24 12:03:25 +0000

    added login user controller message

--- controllers/auth.js
+++ controllers/auth.js
@@ -20,3 +20,29 @@ exports.register = asyncHandler(async(req, res, next) => {
 
     sendTokenResponse(user, 200, res);
 });             
+
+// @desc            Login User
+// @route           POST /api/v1/auth/login
+// @access          Public
+exports.login = asyncHandler(async(req, res, next) => {
+    // Validate user email & password
+    if(!email || !password){
+        return next(new ErrorResponse('Please provide an email and password', 400));
+    }
+
+    // Check for the user
+    const user = await User.findOne({ email }).select('+password');
+
+    if(!user) {
+        return next(new ErrorResponse('Invalid credentials', 401));
+    }
+
+    // Check to ensure that the password matches
+    const isMatch = await user.matchPassword(password);
+
+    if(!isMatch){
+        return next(new ErrorResponse('Invalid credentials', 401));
+    }
+
+    sendTokenResponse(user, 200, res);
+});
