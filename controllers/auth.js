703a998781db8250d49f4a5953d18c5a902968de
marb <marb61a@gmail.com>
2019-12-19 12:11:35 +0000

    added auth controller update password and get logged in user methods

--- controllers/auth.js
+++ controllers/auth.js
@@ -63,10 +63,23 @@ exports.logout = asyncHandler(async(req, res, next) => {
         });
 });
 
+// @desc            Get current logged in User
+// @route           POST /api/v1/auth/me
+// @access          Private
+exports.getMe = asyncHandler(async(req, res, next) => {
+    const user = await User.findById(req.user.id);
+
+    res.status(200)
+        .json({
+            success: true,
+            data: user
+        });
+});
+
 // @desc            Update user details
 // @route           PUT api/v1/auth/updatedetails
 // @access          Private
-exports.updateDetails = asyncHandler((req, res, next) => {
+exports.updateDetails = asyncHandler(async(req, res, next) => {
     const fieldsToUpdate = {
         name: req.body.name,
         email: req.body.email
@@ -82,4 +95,20 @@ exports.updateDetails = asyncHandler((req, res, next) => {
         success: true,
         data: user
     });
+});
+
+// @desc            Update password
+// @route           PUT /api/v1/auth/updatepassword
+// @access          Private
+exports.updatePassword= asyncHandler(async(req, res, next) => {
+    const user = await User.findById(req.user.id).select('+password');
+
+    // Check the current password
+    if(!(await user.matchPassword(req.body.currentPassword))){
+        return next(new ErrorResponse('Password is incorrect', 401));
+    }
+
+    user.password = req.body.password;
+    await user.save();
+    sendTokenResponse(user, 200, res);
 });
