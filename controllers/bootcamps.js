85faa2d303d1bcf26965536e39b097ea7d007665
martin <marb61a@gmail.com>
2020-02-02 13:02:32 +0000

    updated  upload bootcamp photo method in bootcamps controller

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -149,5 +149,51 @@ exports.bootcampPhotoUpload = asyncHandler(async (req, res, next) => {
             )
         );
     }
+
+    if(!req.files) {
+        return next(
+            new ErrorResponse(`Please upload a file`, 400)
+        );
+    }
+
+    const file = req.files.file;
+
+    // Ensure that the file is an image
+    if (!file.mimetype.startsWith('image')) {
+        return next(new ErrorResponse(`Please upload an image file`, 400));
+    }
     
+    // Check the filesize
+    if (file.size > process.env.MAX_FILE_UPLOAD) {
+        return next(
+            new ErrorResponse(
+                `Please upload an image less than ${process.env.MAX_FILE_UPLOAD}`, 400
+            )
+        );
+    }
+
+    // Create a custom filename
+    file.name = `photo_${bootcamp._id}${path.parse(file.name).ext}`;
+
+    file.mv(`${process.env.FILE_UPLOAD_PATH}/${file.name}`, async err => {
+        if(err) {
+            console.error(err);
+            return next(
+                new ErrorResponse(
+                    `Problem with file upload`, 500
+                )
+            )
+        }
+
+        await Bootcamp.findByIdAndUpdate(req.params.id, {
+            photo: file.name
+        });
+
+        res.status(200)
+            .json({
+                success: true,
+                data: file.name
+            });
+    });
+
 });
