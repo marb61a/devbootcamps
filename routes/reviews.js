707f1f5b43791f9ac8bece0deb329578f037db21
martin <marb61a@gmail.com>
2020-02-02 13:15:43 +0000

    Added user and review routes

--- routes/reviews.js
+++ routes/reviews.js
@@ -14,4 +14,15 @@ const router = express.Router({ mergeParams: true });
 const advancedResults = require('../middleware/advancedResults');
 const { protect, authorize } = require('../middleware/auth');
 
+router  
+    .route('/')
+    .get(
+        advancedResults(Review, {
+            path: 'bootcamp',
+            select: 'name description'
+        }),
+        getReviews
+    )
+    .post(protect, authorize('user', 'admin'), addReview);    
+
 module.exports = router;
