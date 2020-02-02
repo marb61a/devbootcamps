7f40b371cd30c8ea6c56115bc267c4de6a3d936b
martin <marb61a@gmail.com>
2020-02-02 20:09:48 +0000

    Added course and review routes

--- routes/courses.js
+++ routes/courses.js
@@ -16,7 +16,19 @@ const { protect, authorize } = require('../middleware/auth');
 
 router
     .route('/')
-    .get(advancedResults(User), getUsers)
-    .post(createUser);
+    .get(
+        advancedResults(Course, {
+            path: 'bootcamp',
+            select: 'name description'
+        }),
+        getCourses
+    )
+    .post(protect, authorize('publisher', 'admin'), addCourse);
+
+router
+    .route('/:id')
+    .get(getCourse)
+    .put(protect, authorize('publisher', 'admin'), updateCourse)
+    .delete(protect, authorize('publisher', 'admin'), deleteCourse);
 
 module.exports = router;
