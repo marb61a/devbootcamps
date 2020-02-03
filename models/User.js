587411d154ace5450c9b5e04ff87b3f6a1957470
martin <marb61a@gmail.com>
2020-02-03 20:33:05 +0000

    Updated User model

--- models/User.js
+++ models/User.js
@@ -46,4 +46,33 @@ UserSchema.pre("save", async function(next) {
     this.password = await bcrypt.hash(this.password, salt);
 });
 
+// Sign JWT and return
+UserSchema.methods.getSignedJwtToken = function() {
+    return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRE
+    });
+};
+  
+// Match user entered password to hashed password in database
+UserSchema.methods.matchPassword = async function(enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+  
+// Generate and hash password token
+UserSchema.methods.getResetPasswordToken = function() {
+    // Generate token
+    const resetToken = crypto.randomBytes(20).toString('hex');
+  
+    // Hash token and set to resetPasswordToken field
+    this.resetPasswordToken = crypto
+        .createHash('sha256')
+        .update(resetToken)
+        .digest('hex');
+  
+    // Set expire
+    this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
+  
+    return resetToken;
+  };  
+
 module.exports = mongoose.model('User', UserSchema);
