a628ed9a40da985700f6ba25713c89f1707fdb82
martin <marb61a@gmail.com>
2020-02-04 20:59:37 +0000

    added sendMail util and updated auth middleware

---
+++ utils/sendEmail.js
@@ -0,0 +1,13 @@
+const nodemailer = require('nodemailer');
+
+const sendEmail = async options => {
+    const transporter = nodemailer.createTransport({
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        auth: {
+            user: process.env.SMTP_EMAIL,
+            pass: process.env.SMTP_PASSWORD            
+        }
+    })
+    
+}
